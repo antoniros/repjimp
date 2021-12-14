3ffedb77a75998f92c1a603c6b5910f0bf87ea94
Antoni <antoni.rosiecki@gmail.com>
2021-12-14 18:52:25 +0100

    zmiana elementu glownego 2

--- src/gauss.c
+++ src/gauss.c
@@ -1,34 +1,47 @@
 #include "gauss.h"
-
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-
 int eliminate(Matrix *mat, Matrix *b){
 
         int n = mat->r;
+	int mx, w, k;
+	double *temp;
+	double tempb;
         double **a = mat->data;
         double **wektb = b->data;
         double wspolczynnik;
-
-        for(int s=0; s<n; s++)
+	
+        for(w=0; w<n; w++)
         {
-                if( a[s][s] == 0 ) return 1;
+                if( a[w][w] == 0 ) return 1;
         }
 
-        for(int w=0; w < n-1; w++)
+        for(w=0; w < n; w++)
         {
-                for(int k= w+1; k < n; k++)
+		mx = w;
+		for(k = w + 1; k < n; k++)
+			if(a[k][w] > a[mx][w])
+				mx = k;
+		if (mx != k) {
+			temp = a[w];
+			a[w] = a[mx];
+			a[mx] = temp;
+			tempb = wektb[w][0];
+			wektb[w][0] = wektb[mx][0];
+			wektb[mx][0] = tempb;
+		}		
+                for(int k= w+1; k<n; k++)
                 {
                         wspolczynnik = a[k][w]/ a[w][w];
 
                         for(int p=0; p<n; p++)
                         {
                                 a[k][p] = a[k][p] - wspolczynnik * a[w][p];
-                        }
+                                wektb[k][0] = wektb[k][0] - wspolczynnik * wektb[w][0];
 
-                        wektb[k][0] = wektb[k][0] - wspolczynnik * wektb[w][0];
+                        }
                 }
         }
 
@@ -37,3 +50,4 @@ int eliminate(Matrix *mat, Matrix *b){
 
         return 0;
 }
+                                                                                                 
