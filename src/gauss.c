9cd70561d4c3bff632817f5e578009ef673d03a3
fsosn <95348487+fsosn@users.noreply.github.com>
2021-12-14 19:12:53 +0100

    Update gauss.c
    rozwiazanie konfliktu

--- src/gauss.c
+++ src/gauss.c
@@ -1,56 +1,13 @@
 #include "gauss.h"
-<<<<<<< HEAD
-=======
 #include <math.h>
 
->>>>>>> 6c0263ce6d6dfd1d2f8b0580dde3877d20eb41ff
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
+
 int eliminate(Matrix *mat, Matrix *b){
 
-<<<<<<< HEAD
-        int n = mat->r;
-	int mx, w, k;
-	double *temp;
-	double tempb;
-        double **a = mat->data;
-        double **wektb = b->data;
-        double wspolczynnik;
-	
-        for(w=0; w<n; w++)
-        {
-                if( a[w][w] == 0 ) return 1;
-        }
-
-        for(w=0; w < n; w++)
-        {
-		mx = w;
-		for(k = w + 1; k < n; k++)
-			if(a[k][w] > a[mx][w])
-				mx = k;
-		if (mx != k) {
-			temp = a[w];
-			a[w] = a[mx];
-			a[mx] = temp;
-			tempb = wektb[w][0];
-			wektb[w][0] = wektb[mx][0];
-			wektb[mx][0] = tempb;
-		}		
-                for(int k= w+1; k<n; k++)
-                {
-                        wspolczynnik = a[k][w]/ a[w][w];
-
-                        for(int p=0; p<n; p++)
-                        {
-                                a[k][p] = a[k][p] - wspolczynnik * a[w][p];
-                                wektb[k][0] = wektb[k][0] - wspolczynnik * wektb[w][0];
-
-                        }
-                }
-        }
-=======
 	int n = mat->r;
 	double **a = mat->data; //macierz A
 	double **wektb = b->data; //wektor b
@@ -74,7 +31,6 @@ int eliminate(Matrix *mat, Matrix *b){
 				max = x;
 				m = k;
 			}
->>>>>>> 6c0263ce6d6dfd1d2f8b0580dde3877d20eb41ff
 
 		}
 		if( m != w )
@@ -118,4 +74,3 @@ int eliminate(Matrix *mat, Matrix *b){
 
 	return 0;
 }
-                                                                                                 
